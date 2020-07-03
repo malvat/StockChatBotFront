5735f269ba337fcbc00e3577ce5be754e7271066
malvat <malvatanim@gmail.com>
2020-07-03 19:27:34 -0400

    set chat UI for the application

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import { Widget, addResponseMessage } from 'react-chat-widget';
+import 'react-chat-widget/lib/styles.css';
+import { render } from '@testing-library/react';
+
+const handleNewUserMessage = (newMessage) => {
+  console.log(newMessage);
+  addResponseMessage("hi ");
+
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  return <Widget handleNewUserMessage={ handleNewUserMessage } />;
 }
 
 export default App;
